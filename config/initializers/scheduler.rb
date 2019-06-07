ae8f4728ebcb9bc1c26bbe91c6b1242256e5bec0
Greg Kostin <gkostin@umich.edu>
2019-06-07 06:22:00 -0400

    schedulaer.cron '0 0 0 * *' daily (#13)

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -2,14 +2,15 @@
 
 require 'rufus-scheduler'
 
-# Let's use the rufus-scheduler singleton
-scheduler = Rufus::Scheduler.singleton
-
 # Do not schedule when Rails is run from its console, for a test/spec, or from a Rake task
 # return if defined?(Rails::Console) || Rails.env.test? || File.split($0).last == 'rake'
+# Do not schedule unless Rails is run in production mode.
 return unless Rails.env.production?
 
 Rails.logger.info "Schedule Task #{Time.now}"
+
+# Let's use the rufus-scheduler singleton
+scheduler = Rufus::Scheduler.singleton
 
 # Heartbeat - do something every minute
 # scheduler.every '1m' do
@@ -19,15 +20,15 @@ Rails.logger.info "Schedule Task #{Time.now}"
 # end
 
 # Hourly - do something every hour
-scheduler.every '1h' do
-  NotifierMailer.administrators("Hourly").deliver_now
-  Rails.logger.info "Hourly #{Time.now}"
-  Rails.logger.flush
-end
+# scheduler.every '1h' do
+#   NotifierMailer.administrators("Hourly").deliver_now
+#   Rails.logger.info "Hourly #{Time.now}"
+#   Rails.logger.flush
+# end
 
-# Daily - do something every day, five minutes after midnight
+# Daily - do something every day
 # (see "man 5 crontab" in your terminal)
-scheduler.cron '5 0 * * *' do
+scheduler.cron '0 0 * * *' do
   NotifierMailer.administrators("Daily").deliver_now
   Rails.logger.info "Daily #{Time.now}"
   Rails.logger.flush
