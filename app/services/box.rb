5492d7bd76bcbb26e033642d1efd12e14cb0a4cb
Greg Kostin <gkostin@umich.edu>
2019-06-24 13:01:08 -0400

    remove box

--- app/services/box.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require "box/file"
-require "box/folder"
-require "box/service"
-
-module Box
-end
--- spec/support/mock_boxr.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "mock_boxr/item"
-require_relative "mock_boxr/file"
-require_relative "mock_boxr/folder"
-require_relative "mock_boxr/client"
-
-module MockBoxr
-end
