79739d6a1fde2397950ab148b3340378565f9ae8
Greg Kostin <gkostin@umich.edu>
2026-02-23 15:24:43 -0500

    mostly harmless

--- image/mysql/build-image.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-OPT_TAG="-t mysql:latest"
-echo "${OPT_TAG}"
-ARG_PATH=$(dirname "$0")
-echo "${ARG_PATH}"
-docker build ${OPT_TAG} ${ARG_PATH}
--- k8s/nginx-demo/delete.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-kubectl delete -f nginx-node-port-service.yaml
-kubectl delete -f nginx-deployment.yaml
-kubectl delete -f nginx-demo-resource-quota.yaml
-kubectl delete -f nginx-demo-namespace.yaml
